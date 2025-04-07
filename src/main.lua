41f4c41766f32d02aa007280dd5317aa57dc067e
path <patheticcs@gmail.com>
2025-04-06 17:50:33 -0700

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/Soluna-API/refs/heads/main/src/api/main.lua",true))()
+if not getgenv()._soluna_api_loaded and game.PlaceId == 17625359962 then
+    getgenv()._soluna_api_loaded = true
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/Soluna-API/refs/heads/main/src/api/main.lua", true))()
+end
