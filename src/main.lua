390c678ce1e620fbcde62bde365c92b5a9ce7166
path <patheticcs@gmail.com>
2025-04-07 11:23:31 -0700

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,1 @@
-if not getgenv()._soluna_api_loaded then
-    getgenv()._soluna_api_loaded = true
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/Soluna-API/refs/heads/main/src/api/main.lua", true))()
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/Soluna-API/refs/heads/main/main.lua", true))()
