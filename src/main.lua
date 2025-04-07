60bebe2e4448367c191d8d7ade030799b2f02c26
path <patheticcs@gmail.com>
2025-04-07 11:35:30 -0700

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/Soluna-API/refs/heads/main/main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/Soluna-API/refs/heads/main/src/api/main.lua",true))()
